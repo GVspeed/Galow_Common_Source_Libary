a2da9332932bf1d2ee25a01f46486e2ce8c64fde
GVspeed <yannijepsen@gmail.com>
2024-07-08 21:42:58 +0200

    commit 21:43 8th July edited main.py - added approute with flask

--- main.py
+++ main.py
@@ -2,5 +2,19 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+@app.route("/user/<user_id>")
+def user_id(user_id):
+    user_data = {
+        "user_id": 0,
+        "request": "datetime",
+        "token": "124",
+    }
+
+    extra = request.args.get("extra")
+    if extra:
+        user_data["extra"] = extra
+
+    return jsonify(user_data), 200
+
 if __name__ == "__main__":
     app.run(debug=True)
